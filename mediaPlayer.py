7cc84974d183380319b2128580fa0a672ae1ea54
Muhammad Arslan <123152776+arslan3120@users.noreply.github.com>
2023-04-01 21:24:51 +0500

    Add files via upload

---
+++ mediaPlayer.py
@@ -0,0 +1,13 @@
+from tkinter import *
+import tkinter as tk
+from tkinter import ttk, filedialog
+from pygame import mixer
+import os
+
+root=Tk()
+root.title("Media Player")
+root.geometry("920x670+290+85")
+root.config(bg="#0f1a2b")
+root.resizable(False,False)
+
+root.mainloop()
